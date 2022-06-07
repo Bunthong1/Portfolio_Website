0f70911061e15999191387069ff3b4c4352e9ec7
bunthong115 <100556916+Bunthong1@users.noreply.github.com>
2022-06-07 17:48:40 +1200

    php contact form

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+
+    if(isset($_POST['name']) && $_POST['name'] != ''){
+
+        if(first_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            $userName = $_POST['name'];
+            $userEmail = $_POST['email'];
+            $userPhone = $_POST['phone'];
+            $userMessage = $_POST['message'];
+
+            $to = "cheabunthong168@gmail.com";
+            $subject = "Contact Form Info";
+            $body = "";
+
+            $body .= "From : ".$userName. "\r\n";
+            $body .= "Email : ".$userEmail. "\r\n";
+            $body .= "Phone : ".$userPhone. "\r\n";
+            $body .= "Message : ".$userMessage. "\r\n";
+
+            mail($to, $subject, $body);
+
+            $message_sent = true
+        }
+        else{
+            $message_sent = false
+        }
+
+        
+    }
+
+    
+?>
